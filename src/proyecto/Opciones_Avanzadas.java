0c08482b10f8d87517987e3abe326d7695af0570
JoseEduardo-code <s24030112@itsch.edu.mx>
2026-03-19 13:37:11 -0600

    otro avanze

--- src/proyecto/Opciones_Avanzadas.java
+++ src/proyecto/Opciones_Avanzadas.java
@@ -190,7 +190,6 @@ public class Opciones_Avanzadas extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-
         this.setVisible(false);
         
     }//GEN-LAST:event_jButton1ActionPerformed
